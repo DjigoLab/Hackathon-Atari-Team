9b9f2e267d2599418d6061b34d9e17e60926827b
rogberg <nedfall@gmail.com>
2018-04-27 12:01:19 +0100

    updated sw cache

--- hackathon2018-static/sw.js
+++ hackathon2018-static/sw.js
@@ -2,13 +2,13 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/styles/main.css',
-       '/scripts/main.min.js',
-       '/sounds/airhorn.mp3'
+      '/',
+      '/index.html',
+      '/voice.html',
+      '/video.html',
+      '/contact_us.html',
+      '/data.html',
+      '/company_profile.html',
      ]);
    })
  );
