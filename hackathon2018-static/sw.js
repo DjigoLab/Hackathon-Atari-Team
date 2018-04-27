cf907d458dae4ab4bf883c59873f3988c979891f
Sejas <antonio@sejas.es>
2018-04-27 11:58:34 +0100

    fetch service worker

--- hackathon2018-static/sw.js
+++ hackathon2018-static/sw.js
@@ -15,7 +15,30 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log('fetching ...',event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
 
-  console.log(event.request.url);
-  
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
   });
+}
